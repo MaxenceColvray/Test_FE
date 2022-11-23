af0942b23e32feab3375622a09616d1783e67021
MaxenceColvray <105800153+MaxenceColvray@users.noreply.github.com>
2022-11-23 11:29:02 +0100

    /

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
+
+mongoose.connect(process.env.MONGODB_connection_string,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 const app = express();
 
 app.use((req, res, next) => {
